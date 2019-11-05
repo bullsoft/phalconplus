5ecf4e1f7ff616f4845e2350bc45efc103db32ee
Weigang <guweigang@bullsoft.org>
2019-11-05 13:27:53 +0800

    bump version 1.1.1; remove php5-support; refactor logger

--- php5-ext/phalconplus/base/abstractrequest.zep.h
+++
@@ -1,5 +0,0 @@
-
-extern zend_class_entry *phalconplus_base_abstractrequest_ce;
-
-ZEPHIR_INIT_CLASS(PhalconPlus_Base_AbstractRequest);
-
--- php5-ext/phalconplus/enum/orderbydirection.zep.h
+++
@@ -1,5 +0,0 @@
-
-extern zend_class_entry *phalconplus_enum_orderbydirection_ce;
-
-ZEPHIR_INIT_CLASS(PhalconPlus_Enum_OrderByDirection);
-
